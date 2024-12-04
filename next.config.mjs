c0405c1de87b92aedf4a80d1a41e261cfd926712
moizpashadev <moizpasha.developer@gmail.com>
2024-12-04 15:48:15 +0500

    bigfix-3

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,9 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
-
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      });
-  
-      return config;
-    },
-    webpack(config, { isServer }) {
-      if (!isServer) {
-        config.resolve.fallback = {
-          fs: false,
-        };
-      }
-      return config;
-    },
-  };
-  
-  export default nextConfig;
-  
+// next.config.mjs
+export default {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack(config) {
+    // Avoid overriding default CSS handling
+    return config;
+  },
+};
